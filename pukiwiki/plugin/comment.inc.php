c23a07c0d7523c396cc9307bf266829117e5f3ae
matanki.saito <matanki.saito@gmail.com>
2020-04-12 08:29:33 +0900

    コメントからURLを除外

--- pukiwiki/plugin/comment.inc.php
+++ pukiwiki/plugin/comment.inc.php
@@ -37,6 +37,9 @@ function plugin_comment_action()
 	}
 	if ($vars['msg'] == '') return array('msg'=>'', 'body'=>''); // Do nothing
 
+	if (preg_match('/.*http.*/i',$vars['msg'])) return array('msg'=>'', 'body'=>''); // Do nothing
+	if (preg_match('/.*http.*/i',$head)) return array('msg'=>'', 'body'=>''); // Do nothing
+
 	$comment  = str_replace('$msg', $vars['msg'], PLUGIN_COMMENT_FORMAT_MSG);
 	if(isset($vars['name']) || ($vars['nodate'] != '1')) {
 		$_name = (! isset($vars['name']) || $vars['name'] == '') ? $_no_name : $vars['name'];
