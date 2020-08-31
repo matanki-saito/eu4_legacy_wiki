935033cac4c9ec86cf16437f6e97ad84982ee2b9
matanki.saito <matanki.saito@gmail.com>
2020-09-01 06:52:17 +0900

    ga追加

--- pukiwiki/skin/pukiwiki.skin.php
+++ pukiwiki/skin/pukiwiki.skin.php
@@ -132,6 +132,15 @@ window.addEventListener('load', function() {
 
 </script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-176921702-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-176921702-1');
+</script>
 
 <?php echo $head_tag ?>
 </head>
